4458bac2a04d742d06d544ffd068968714f0832f
pablo <pabloocampopita@gmail.com>
2019-07-09 13:17:20 -0400

    something

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const bodyParser = require('body-parser')
 const app = express();
 const urlEncoded = bodyParser.urlencoded({extended: false})
 const dummyData = [{taskItem: "Week 1" },{taskItem: "Week 2"},{taskItem: "Week 3"}];
+const mysql      = require('mysql');
+
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'admin',
+  password : 'pablo',
+  database : 'websiteblog'
+});
+
+
+
+db.connect(function(err){
+    if (err) throw err
+    console.log("My SQL is connected, looking good")
+});
+
 app.set("view engine","ejs");
 
 app.use(express.static('./public'));
@@ -14,6 +30,30 @@ app.post('/blog', urlEncoded, (req, res) => {
     incomingItem.taskItem = req.body.task;
     dummyData.push(incomingItem);
     res.redirect('/blog');
+
+app.get("/createposttable", (req, res) => {
+    let sql = 'CREATE TABLE users (ID int NOT NULL AUTO_INCREMENT, title varchar(255), body varchar(255), PRIMARY KEY (ID))'
+    db.query( sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("Table created ...");
+    });
+});
+
+
+app.get("/addpost", (req, res) => {
+     let incomingItem = {};
+     let post = {title: "", body: ""}
+     let sql = 'INSERT INTO users SET ?';
+      db.query( sql, post,  (err, result) => {
+         if (err) throw err;
+         console.log(result);
+          res.send("first post add");
+      })
+  });
+
+
+
   });
   app.delete("/destroyer", () => {
       console.log(req.params.id);
@@ -32,8 +72,22 @@ app.get('/blog', (req, res) => {
     res.render('blog', {taskToDo: dummyData});
 });
 
+app.get('/mblog', (req, res) => {
+
+    res.render('mblog');
+});
 
 
+
+app.get("/moreblogs", (req, res) => {
+    let sql = 'CREATE TABLE postsss (ID int NOT NULL AUTO_INCREMENT, title varchar(255), body varchar(255), PRIMARY KEY (ID))'
+    db.query( sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("Table created ...");
+        res.redirect('/blog')
+    });
+});
 
 
 
