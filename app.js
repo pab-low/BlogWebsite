38e8aef689b6af15e790e32752a518852ebb2faa
pablo <pabloocampopita@gmail.com>
2019-07-09 14:54:23 -0400

    adding database

--- app.js
+++ app.js
@@ -24,69 +24,44 @@ app.set("view engine","ejs");
 
 app.use(express.static('./public'));
 
-app.post('/blog', urlEncoded, (req, res) => { 
-    let incomingItem = {};
-    console.log("working")
-    incomingItem.taskItem = req.body.task;
-    dummyData.push(incomingItem);
-    res.redirect('/blog');
+// ############### ROUTES ##############
 
-app.get("/createposttable", (req, res) => {
-    let sql = 'CREATE TABLE users (ID int NOT NULL AUTO_INCREMENT, title varchar(255), body varchar(255), PRIMARY KEY (ID))'
-    db.query( sql, (err, result) => {
+
+
+
+
+
+app.get('/', (req, res) => {
+    let sql = 'SELECT * FROM profile'
+    db.query(sql, function (err, results) {
         if (err) throw err;
-        console.log(result);
-        res.send("Table created ...");
-    });
+        res.render('blog', {taskToDo: results});
+    }); 
 });
 
 
-app.get("/addpost", (req, res) => {
-     let incomingItem = {};
-     let post = {title: "", body: ""}
-     let sql = 'INSERT INTO users SET ?';
-      db.query( sql, post,  (err, result) => {
-         if (err) throw err;
-         console.log(result);
-          res.send("first post add");
-      })
-  });
 
-
-
-  });
-  app.delete("/destroyer", () => {
-      console.log(req.params.id);
-      dummyData.splice(req.params.id, 1);
-      console.log("hitting delete route");
-      res.redirect('/blog')
-  });
-  app.delete("/blog/:id", (req, res) => {
-      console.log("its getting hit")
-      dummyData.splice(req.params.id, 1);
-      res.json(dummyData)
-  });
-
-app.get('/blog', (req, res) => {
-
-    res.render('blog', {taskToDo: dummyData});
-});
 
 app.get('/mblog', (req, res) => {
 
     res.render('mblog');
 });
 
+app.get('/blog', (req, res) => {
+
+    res.render('blog', {taskToDo: results})
+});
 
 
-app.get("/moreblogs", (req, res) => {
-    let sql = 'CREATE TABLE postsss (ID int NOT NULL AUTO_INCREMENT, title varchar(255), body varchar(255), PRIMARY KEY (ID))'
-    db.query( sql, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send("Table created ...");
-        res.redirect('/blog')
-    });
+app.post('/blog', urlEncoded, (req, res) => {
+    let task = req.body
+    console.log(task)
+    let sql = 'INSERT INTO profile SET ?';
+    db.query(sql, task, function (err, results) {
+         if (err) throw err;
+         console.log(results)
+         res.redirect('/')
+        });
 });
 
 
@@ -94,18 +69,17 @@ app.get("/moreblogs", (req, res) => {
 
 
 
+app.delete("/blog/:id", (req, res) => {
+    let sql = 'DELETE FROM profile WHERE ID=' + req.params.id;
+    db.query(sql,(err, result) =>{
+        if(err) throw err;
+        console.log(result);
+        res.json(result)
+       })
+    });
 
-
-
-
-
-
-
-
-
-
-app.listen(3000, (err) => {
+app.listen(3000, function(err){
     if (err)
         console.log(err)
     console.log('Server is live on port 3000')
-})  
+})
